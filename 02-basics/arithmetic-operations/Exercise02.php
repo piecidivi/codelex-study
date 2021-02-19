b7e4a06a652b6428fcc35d9ff9c5545ed8032571
Peteris Zarans <peteris.zarans@gmail.com>
2021-02-20 01:53:49 +0200

    Updated Exercise02

--- 02-basics/arithmetic-operations/Exercise02.php
+++ 02-basics/arithmetic-operations/Exercise02.php
@@ -3,7 +3,7 @@
 echo "CheckOddEven" . PHP_EOL;
 $number = readline("Please enter number: ");
 if (is_numeric($number) && gettype($number + 0) === "integer") {
-    echo intval($number) % 2 === 0 ? "Even Number\n" : "Odd Number\n";
+    echo intval($number) & 1 ? "Odd Number\n" : "Even Number\n";
 } else {
     echo "Not a number or integer!" . PHP_EOL;
 }
