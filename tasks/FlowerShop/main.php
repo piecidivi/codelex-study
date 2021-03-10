4383c4b882cbfae5fd4f3c62fe2de4fc70e61090
Peteris Zarans <peteris.zarans@gmail.com>
2021-03-10 03:28:59 +0200

    Flower Shop final update

--- tasks/FlowerShop/main.php
+++ tasks/FlowerShop/main.php
@@ -1,13 +1,5 @@
 <?php
 
-// We will have our Flower Shop here
-// Shop.php is Flower Shop
-// SillyTrade.php, RegularTrade.php, and HustleTrade.php are wholesalers
-// They all extend abstract Warehouse class
-// Wholesalers implement Trade interface
-// Flower object itself is described by Flower class
-// Warehouse class has array property as Flowers collection
-// View class is formatting output
 
 require_once "Flower.php";
 require_once "Warehouse.php";
